5516b4c00f2399e030ad82f62b587c854dea4786
Mannan-Goyal <mannang6@gmail.com>
2021-07-09 16:43:49 +0530

    different logger

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
-const winston = require('winston');
-const cors = require('cors')
-require('dotenv').config();
-
-const Email = require('./models/emails');
+const cors = require('cors');
 const { default: axios } = require('axios');
+const { logger } = require('./logger');
+const Email = require('./models/emails');
+
+/**
+ * Global configuration
+ */
+const secretKey = process.env.CAPTCHA;
 
 const app = express();
 app.use(cors());
 
+/** database connections and configurations */
 mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,52 +27,46 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.printf((info) => `${info.message}`),
-  transports: [
-    new winston.transports.File({
-      filename: path.join(__dirname, 'error.log'),
-      level: 'info',
-    }),
-  ],
-});
-
-const secretKey = process.env.CAPTCHA;
-
-app.post('/rsvp', (req, res) => {
+/** route to handle email and captcha data from user */
+app.post('/rsvp', async (req, res) => {
   if (!req.body.captcha) {
     logger.error(`Captcha wasn't supplied in body`);
-    res.json({ success: false, msg: 'Capctha is not checked' });
+    return res.json({ success: false, msg: 'Capctha is not checked' });
   }
+
   const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`;
-  axios.get(verifyUrl,async (err, response, body) => {
-    if (err) {
-      logger.error(`Error During Captcha: ${err}`);
-    }
-    body = JSON.parse(body);
-    if (!body.success && body.success === undefined) {
+  try {
+    const request = await axios.get(verifyUrl);
+    const { data } = request;
+
+    if (!data.success || data.success === undefined) {
+      logger.warn(`Captcha verification failed: ${JSON.stringify(data)}`);
       return res.json({ success: false, msg: 'captcha verification failed' });
     }
-    if (body.score < 0.5) {
+
+    if (data.score < 0.5) {
       return res.json({
         success: false,
         msg: 'you might be a bot, sorry!',
       });
     }
+
+    const email = req.body.email?.toString();
+    logger.info(`Adding email to db, Email: ${email}`);
+
     try {
-      const email = req.body.email?.toString();
-      logger.info(`Adding email to db, Email: ${email}`);
-      await Email.create({
-        email,
-      });     
-    } catch (e) {
-      res.status(500).send(e);
-      logger.error(`Error adding Email to db: ${e}`);
+      await Email.create({ email });
+    } catch (err) {
+      logger.error(`Error adding email to db: ${err}`);
+      return res.json({ success: false, msg: 'email already in list' });
     }
-  });
+
+    return res.json({ success: true, msg: 'email added' });
+  } catch (e) {
+    logger.error(`Captcha verification failed: ${e}`);
+  }
 });
 
 app.listen(process.env.PORT, () => {
-  logger.info(`Server Started on port ${process.env.PORT}`);
+  logger.info(`Server started on port ${process.env.PORT}`);
 });
